71b1f03bd89d76ba632326d7faa5355e3ab589c5
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-08-27 15:45:34 -0500

    Created NavBar animations
    Created Navbar animations for "Home", "About", "Projects", and "Articles". A bar appears on the bottom when the mouse hovers over them.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,39 @@ import React from 'react';
 import Link from 'next/link';
 import Logo from './Logo';
 
+// CustomLink Component
+const CustomLink = ({ href, title, className = "" }) => {
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span className='
+                h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 
+                group-hover:w-full transition-[width] ease duration-300
+            '>&nbsp;</span>
+        </Link>
+    );
+};
+
+// NavBar Component
 const NavBar = () => {
     return (
-        <header className='w-full px-32 py-8 font-medium flex items-center justify-between relative'>
+        <header className='
+            w-full px-32 py-8 font-medium flex items-center justify-between relative
+        '>
             <nav className='flex space-x-4'>
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
+                <CustomLink href="/" title="Home" className='mr-4' />
+                <CustomLink href="/about" title="About" className='mx-4' />
             </nav>
 
-            <div className='absolute inset-x-1/2 transform -translate-x-1/2 flex justify-center'>
+            <div className='
+                absolute inset-x-1/2 transform -translate-x-1/2 flex justify-center
+            '>
                 <Logo />
             </div>
 
             <nav className='flex space-x-4'>
-            <Link href="/projects">Projects</Link>
-            <Link href="/articles">Articles</Link>
+                <CustomLink href="/projects" title="Projects" className='mx-4' />
+                <CustomLink href="/articles" title="Articles" className='ml-4' />
             </nav>
         </header>
     );
