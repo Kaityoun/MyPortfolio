ed28ffe18d88d1cadcde182f1c798f1c25ffd5fb
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-10-22 17:40:47 -0500

    Added Icons to NavBar
    Added icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 import Logo from './Logo';
+import {useRouter} from 'next/router'
+import { LinkedinIcon, GithubIcon, LinkedInIcon } from './Icons';
 
 // CustomLink Component
 const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
+    console.log(router)
     return (
         <Link href={href} className={`${className} relative group`}>
             {title}
-            <span className='
-                h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 
+            <span className={`
+                h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 
                 group-hover:w-full transition-[width] ease duration-300
-            '>&nbsp;</span>
+                ${router.asPath === href ? 'w-full' : 'w-0'}
+            `}>
+                &nbsp;
+            </span>
         </Link>
     );
 };
@@ -26,16 +33,23 @@ const NavBar = () => {
                 <CustomLink href="/about" title="About" className='mx-4' />
             </nav>
 
-            <div className='
-                absolute inset-x-1/2 transform -translate-x-1/2 flex justify-center
-            '>
-                <Logo />
-            </div>
-
             <nav className='flex space-x-4'>
                 <CustomLink href="/projects" title="Projects" className='mx-4' />
                 <CustomLink href="/articles" title="Articles" className='ml-4' />
+
+                <Link href = "/" target = {"_blank"}>
+                    <LinkedInIcon/>
+                </Link>
+                
+                <Link href = "/" target = {"_blank"}>
+                    <GithubIcon/>
+                </Link>
             </nav>
+
+
+        <div className = 'absolute left-[50%] top-2 translate-x-[-50%'>
+        <Logo />
+        </div>
         </header>
     );
 };
