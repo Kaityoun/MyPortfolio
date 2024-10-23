015c345dbbd3c6cba20e602668452a6f5bc9f43b
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2024-10-22 22:23:20 -0500

    Created all footers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
 
-const Footer = ()=> {
-    return(
-        <div>Footer</div>
-    )
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
+            {/* All Rights Reserved */}
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+
+            {/* Built With Love by Kaitlyn */}
+            <div className='flex items-center'>
+            Built with 
+            <span className='text-primary text-2xl px-1'>&#9825;</span> 
+            by Kaitlyn
+            </div>
+
+            {/* Link to Contact */}
+            <Link href='/' className='hover:underline'>Say Hello</Link>
+      </Layout>
+    </footer>
+  )
 }
 
 export default Footer
