21b523eb9b185b23379b16f9d4f10afee8c303d7
Kaityoun <120692265+Kaityoun@users.noreply.github.com>
2025-01-15 10:25:35 -0600

    updated

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,6 +3,19 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import AnimatedText from '@/components/AnimatedText'
 
+const FeaturedArticle = ({img, title, time, summary, link}) => {
+    return(
+        <li>
+            <Link href = {link} target = "_blank" 
+            className = 'w-full cursor-pointer overflow-hidden rounded-lg'>
+            <Image src = {img} alt={title} className = "w-full h-auto"/>
+            </Link>
+            <Link href = {link} target = '_blank'>
+            
+            </Link>
+        </li>
+    )
+}
 const articles = () => {
     return(
         <>
@@ -10,11 +23,12 @@ const articles = () => {
                 <title>Articles</title>
                 <meta name = "description" content= "any description"/>
             </Head>
-            <main>
+            <main className = 'w-full mb-16 flex flex-col items-center justify-center overflow-hidden'>
                 <Layout className = 'pt-16'>
                     <AnimatedText text = "Articles" className='mb-16'/>
-                    <ul>
-                        <li>Featured Articles</li>
+                    <ul className= 'grid grid-col-2 gap-16'>
+                        <li>Featured Article-1</li>
+                        <li>Featured Article-2</li>
                     </ul>
                 </Layout>
             </main>
